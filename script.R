6ea7f62993892bd2626b808c2fed0335ea6a4362
Elyson <elysonpanolino@gmail.com>
2025-01-18 22:50:20 +0800

    Change Data -Version 2!

--- script.R
+++ script.R
@@ -1,11 +1,9 @@
 
-
 # Load Necessary Library
-# Note: run command: install.packages("readr") if not already satisfied
-library(readr)
+library(readxl)
 
 # Load the Dataset
-data <- read_csv("50startup.csv")
+data <- read_excel("50startups.xlsx")
 
 # Build the full multiple linear regression model
 fullmodel <- lm(PROFIT ~ RND + ADMIN + MKT, data=data)
@@ -13,33 +11,50 @@ fullmodel <- lm(PROFIT ~ RND + ADMIN + MKT, data=data)
 # Summarize full model to view the results
 summary(fullmodel)
 
-# Reduced model using only the significant variable (RND)
-reducedmodel <- lm(PROFIT ~ RND, data = data)
+# Reduced model using only the significant variables (RND & MKT)
+reducedmodel <- lm(PROFIT ~ RND+MKT, data = data)
 
 # Summarize reduced model to view the results
 summary(reducedmodel)
 
-# TESTS FOR ASSUMPTIONS OF LINEAR REGRESSION
 
-#Test for  Linearity Linearity plot
+#Check  Linearity 
+
+# Plot residuals vs. fitted values to check linearity
 plot(reducedmodel$fitted.values, residuals(reducedmodel),
-     xlab = "Fitted Values", ylab = "Residuals",
-     main = "Residuals vs Fitted")
+     main = "Residuals vs Fitted",
+     xlab = "Fitted Values",
+     ylab = "Residuals",
+     pch = 20, col = "blue")
 abline(h = 0, col = "red")
 
-
-# Test for  Independence of Residuals:  Durbin-Watson test
-# Note: run command: install.packages("lmtest") if not already satisfied
-
+# Check Independece of Residuals
+# Durbin-Watson test for independence
 library(lmtest)
 dwtest(reducedmodel)
 
+# Check Homoscedasticity
+# Breusch-Pagan test for homoscedasticity
+library(lmtest)
+bptest(reducedmodel)
 
-#Test for Homoscedasticity
-# Scale-location plot
+# Plot Scale-Location (Residuals vs. Square Root of Fitted Values)
 plot(reducedmodel, which = 3)
 
 
+# Check Normality of Residuals
+# Q-Q plot
+qqnorm(residuals(reducedmodel))
+qqline(residuals(reducedmodel), col = "red")
+
+# Shapiro-Wilk test for normality
+shapiro.test(residuals(reducedmodel))
+
+
+# Check For Multicollinearity
+# Variance Inflation Factor (VIF)
+library(car)
+vif(reducedmodel)
 
 
 
